6312e5afd608f000941bee87d01d44f6016718a0
Andrey Grytsenko <andreyapg@gmail.com>
2022-10-24 09:10:55 +0300

    01 Routes 2022-10-24

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::prefix('{locale}')
+    ->where(['locale' => '[azA-Z{2}'])
+    ->group(function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
 
-Auth::routes();
+        Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    });
+
